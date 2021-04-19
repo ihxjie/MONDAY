09554de7aa3d8b518cb73ea2debc6b5c4d0471a9
ihxjie <xingjie1999@gmail.com>
2021-04-19 17:10:23 +0800

    实现接口调用

--- app/src/main/java/com/ihxjie/monday/MainActivity.java
+++ app/src/main/java/com/ihxjie/monday/MainActivity.java
@@ -9,10 +9,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.arcsoft.face.ActiveFileInfo;
+import com.arcsoft.face.ErrorInfo;
+import com.arcsoft.face.FaceEngine;
+import com.arcsoft.face.enums.RuntimeABI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ihxjie.monday.face.activity.ChooseFunctionActivity;
+import com.ihxjie.monday.face.activity.RegisterAndRecognizeActivity;
+import com.ihxjie.monday.face.common.Constants;
+import com.ihxjie.monday.face.fragment.ChooseDetectDegreeDialog;
 import com.ihxjie.monday.mipush.MondayApplication;
 import com.ihxjie.monday.zxing.android.CaptureActivity;
 
@@ -29,6 +38,14 @@ import androidx.navigation.ui.NavigationUI;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
 
     public static List<String> logList = new CopyOnWriteArrayList<String>();
@@ -36,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
     private static final String DECODED_CONTENT_KEY = "codedContent";
     private static final String DECODED_BITMAP_KEY = "codedBitmap";
     private static final int REQUEST_CODE_SCAN = 0x0000;
+
+    private static final String TAG = "MainActivity";
+    private static final int ACTION_REQUEST_PERMISSIONS = 0x001;
+    // 在线激活所需的权限
+    private static final String[] NEEDED_PERMISSIONS = new String[]{
+            Manifest.permission.READ_PHONE_STATE
+    };
+    boolean libraryExists = true;
+    // Demo 所需的动态库文件
+    private static final String[] LIBRARIES = new String[]{
+            // 人脸相关
+            "libarcsoft_face_engine.so",
+            "libarcsoft_face.so",
+            // 图像库相关
+            "libarcsoft_image_util.so",
+    };
+    // 修改配置项的对话框
+    ChooseDetectDegreeDialog chooseDetectDegreeDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +118,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.face:
-
+                activeEngine();
+                startActivity(new Intent(this, RegisterAndRecognizeActivity.class));
                 break;
             default:
                 break;
@@ -129,4 +165,93 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    protected void showToast(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    }
+    protected void showLongToast(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+
+    /**
+     * 权限检查
+     *
+     * @param neededPermissions 需要的权限
+     * @return 是否全部被允许
+     */
+    protected boolean checkPermissions(String[] neededPermissions) {
+        if (neededPermissions == null || neededPermissions.length == 0) {
+            return true;
+        }
+        boolean allGranted = true;
+        for (String neededPermission : neededPermissions) {
+            allGranted &= ContextCompat.checkSelfPermission(this, neededPermission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return allGranted;
+    }
+
+    /**
+     * 激活引擎
+     */
+    public void activeEngine() {
+        if (!libraryExists) {
+            showToast(getString(R.string.library_not_found));
+            return;
+        }
+        if (!checkPermissions(NEEDED_PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, NEEDED_PERMISSIONS, ACTION_REQUEST_PERMISSIONS);
+            return;
+        }
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) {
+                RuntimeABI runtimeABI = FaceEngine.getRuntimeABI();
+                Log.i(TAG, "subscribe: getRuntimeABI() " + runtimeABI);
+
+                long start = System.currentTimeMillis();
+                int activeCode = FaceEngine.activeOnline(MainActivity.this, Constants.APP_ID, Constants.SDK_KEY);
+                Log.i(TAG, "subscribe cost: " + (System.currentTimeMillis() - start));
+                emitter.onNext(activeCode);
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer activeCode) {
+                        if (activeCode == ErrorInfo.MOK) {
+                            showToast(getString(R.string.active_success));
+                        } else if (activeCode == ErrorInfo.MERR_ASF_ALREADY_ACTIVATED) {
+                            showToast(getString(R.string.already_activated));
+                        } else {
+                            showToast(getString(R.string.active_failed, activeCode));
+                        }
+
+                        ActiveFileInfo activeFileInfo = new ActiveFileInfo();
+                        int res = FaceEngine.getActiveFileInfo(MainActivity.this, activeFileInfo);
+                        if (res == ErrorInfo.MOK) {
+                            Log.i(TAG, activeFileInfo.toString());
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        showToast(e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
 }
--- app/src/main/java/com/ihxjie/monday/api/CourseSpider.java
+++
@@ -1,9 +0,0 @@
-package com.ihxjie.monday.api;
-
-import com.ihxjie.monday.bean.Course;
-
-import java.util.List;
-
-public interface CourseSpider {
-    List<Course> QueryFJUTCourses(String username, String password);
-}
